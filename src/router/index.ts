423b9986406c9bf523344403d5bc58c05f16d119
jtdLab <72231111+jtdLab@users.noreply.github.com>
2023-03-05 21:03:40 +0100

    feat: add not found component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="not-found">
+    <h1>Ooops page not found 404. <router-link to="/">Go to Home.</router-link></h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .not-found {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
